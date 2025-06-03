df9d8a2ac842fad010a7a4b8990c2a807b8c3de5
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2025-06-03 15:10:36 +0530

    Update display_progress.py

--- plugins/functions/display_progress.py
+++ plugins/functions/display_progress.py
@@ -22,8 +22,8 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "{0}{1}".format(
-            ''.join(["██" for i in range(math.floor(percentage / 10))]),
-            ''.join(["░░" for i in range(12 - math.floor(percentage / 10))])
+            ''.join(["🟩" for i in range(math.floor(percentage / 10))]),
+            ''.join(["██" for i in range(12 - math.floor(percentage / 10))])
         )
 
         tmp = progress + Translation.PROGRESS.format(
