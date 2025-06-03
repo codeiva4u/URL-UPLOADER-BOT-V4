352d9eeff882bbc52d77bc122eb57fb2666347a4
codeiva4u <153196182+codeiva4u@users.noreply.github.com>
2025-06-03 15:43:55 +0530

    Update display_progress.py

--- plugins/functions/display_progress.py
+++ plugins/functions/display_progress.py
@@ -23,8 +23,8 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
 
         progress = "{0}{1}".format(
             
-            ''.join(["🟦" for i in range(math.floor(percentage / 10))]),
-            ''.join(["⬜️" for i in range(12 - math.floor(percentage / 10))])
+            ''.join(["🟩" for i in range(math.floor(percentage / 5))]),
+            ''.join(["⬜️" for i in range(10 - math.floor(percentage / 5))])
         )
 
         tmp = progress + Translation.PROGRESS.format(
